3aeeef2d5b30d4959821ba325ccd960d094b68a5
Abaadir <abaadir.yasin@student.moringaschool.com>
2023-10-05 00:54:46 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(activity= 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+
+}
+
+saturdayFun("bathe my dog");
+
+function mondayWork(m = 'go to the office'){
+    return `This Monday, I will ${m}.`;
+}
+
+mondayWork('work from home')
+
+function wrapAdjective(wrapper = '*') {
+    return function result(adjective = '||') {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+  }
+
+    
+   
